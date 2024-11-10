15ce797702766b4f18de69d5bdef2a0c833d2bb8
Samuel Fuller <samuelfuller@live.be>
2024-11-11 00:10:23 +0100

    Commmit 4 2nd tutorial working

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -64,7 +64,7 @@ export const UI = () => {
                     <DownloadButton />  
                 </div>
                 <div className="flex flex-col gap-6">
-                    <Assetbox />
+                    <AssetsBox />
                 </div>  
             </div>
         </main>
--- src/store.js
+++ src/store.js
@@ -1,28 +1,31 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
-import PocketBase from 'pocketbase';
+import PocketBase from "pocketbase";
 
 const pocketBaseUrl = import.meta.env.VITE_POCKETBASE_URL;
-if(!pocketBaseUrl){
-    throw new Error("VITE_POCKETBASE_URL is required");
+if (!pocketBaseUrl) {
+  throw new Error("VITE_POCKETBASE_URL is required");
 }
 
-const pb = new PocketBase(pocketBaseUrl);
+export const pb = new PocketBase(pocketBaseUrl);
 
 export const useConfiguratorStore = create((set) => ({
-  categories:[],
+  categories: [],
   currentCategory: null,
   assets: [],
-  bears: 0,
   fetchCategories: async () => {
-    
-const categories = await pb.collection('CustomizationGroups').getFullList({
-    sort: '+position',
-});
-const assets = await pb.collection('CustomizationAssets').getFullList({
-    sort: '-created',
-});
-    set({categories, currentCategory: categories[0], assets});   
-    },
-    setCurrentCategory: (category) => set({currentCategory: category })
+    // you can also fetch all records at once via getFullList
+    const categories = await pb.collection("CustomizationGroups").getFullList({
+      sort: "+position",
+    });
+    const assets = await pb.collection("CustomizationAssets").getFullList({
+      sort: "-created",
+    });
+    categories.forEach((category) => {
+      category.assets = assets.filter((asset) => asset.group === category.id);
+    });
+
+    set({ categories, currentCategory: categories[0], assets });
+  },
+  setCurrentCategory: (category) => set({ currentCategory: category }),
 }));
