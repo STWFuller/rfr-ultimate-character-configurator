4736c8c2ff5d1b4e2229f2fb7c12405c96af1b58
Samuel Fuller <samuelfuller@live.be>
2024-11-16 22:13:02 +0100

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Experience } from "./components/Experience";
 import { UI } from "./components/UI";
 
 function App() {
- return (
- <>
-  <UI />
-  <Canvas
-    camera={{
-      position: [3, 3, 3],
-    }}
-    >
-    <color attach="background" args={["#333333"]} />
-    <OrbitControls />
-    <mesh>
-      <boxGeometry args={[0.5, 0.5, 0.5]}/>
-      <meshNormalMaterial />
-    </mesh>
-    </Canvas>
+  return (
+    <>
+      <UI />
+      <Canvas
+        camera={{
+          position: [-1, 1, 5],
+          fov: 45,
+        }}
+        shadows
+      >
+        <color attach="background" args={["#555"]} />
+        <fog attach="fog" args={["#555", 15, 25]} />
+        <group position-y={-1}>
+          <Experience />
+        </group>
+      </Canvas>
     </>
-  ) ;
-
+  );
 }
 
-export default App
+export default App;
